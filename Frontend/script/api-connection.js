9cd90376a8adb349568e826479141b50e276f1db
Thiago Vasconcelos <pedrosav14@gmail.com>
2023-05-19 20:36:31 -0300

    feat: conexao api inicial commit

---
+++ Frontend/script/api-connection.js
@@ -0,0 +1,27 @@
+const submitBtn = document.querySelector(".submit-btn")
+
+submitBtn.addEventListener('click', () => {
+    const formData = new FormData(form);
+    const object = {};
+    formData.forEach((value, key) => {
+        object[key] = value;
+    });
+    const json = JSON.stringify(object);
+    console.log(json)
+    fetch('https://localhost:8000/novo', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: json
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('API response:', data);
+        // Process the API response as needed
+    })
+    .catch(error => {
+        console.error('API request failed:', error);
+        // Handle the error
+    })
+})
