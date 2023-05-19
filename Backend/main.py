8fbc5646a0ad1f225f5702b229fcf1e6e2453009
Thiago Vasconcelos <pedrosav14@gmail.com>
2023-05-19 20:36:58 -0300

    feat: endpoints implementados

--- Backend/main.py
+++ Backend/main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from models import UsuarioCadastrado
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500/Frontend/"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+cadastrados = []
 
 @app.get("/")
 def home():
-    return {"msg": "testando"}
+    return cadastrados if len(cadastrados) != 0 else {"hello world"}
+
+
+@app.post("/novo")
+def novo_cadastro(novo: UsuarioCadastrado):
+    cadastrados.append(novo)
+
+    return cadastrados
