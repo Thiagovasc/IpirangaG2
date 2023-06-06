237fa8a7de3583c3bec0b3575d3949f243881c82
Thiago Vasconcelos <pedrosav14@gmail.com>
2023-06-05 22:27:22 -0300

    ref: cors and endpoints routing

--- Backend/main.py
+++ Backend/main.py
@@ -1,40 +1,20 @@
-from fastapi import FastAPI, Query
-from models import UsuarioCadastrado, Response
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from typing import List
-from gerando_cadastros import adiciona_cadastros
+from models import UsuarioCadastrado
 
 app = FastAPI()
 
+cadastrados: List[UsuarioCadastrado] = []
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5500/Frontend"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
 
-cadastrados: List[UsuarioCadastrado] = []
+from endpoints import router
 
-@app.get("/")
-def home():
-    return cadastrados if len(cadastrados) != 0 else {"status" : "Lista vazia"}
-
-
-@app.post("/novo")
-def novo_cadastro(novo: UsuarioCadastrado):
-    cadastrados.append(novo)
-
-    return Response(
-        status = "OK",
-        code = 200,
-        mensagem = "Cadastro adicionado com sucesso",
-        result = novo
-    )
-
-
-@app.post("/gerar-cadastros")
-def gerar_cadastros(cadastros_request: int = Query(..., ge=1)):
-    return adiciona_cadastros(cadastros_request)
-
-
+app.include_router(router)
